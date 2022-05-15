51a3cc4209cccdeee660ea5e2a4f8d1db2868772
Ivan Akulov <mail@iamakulov.com>
2022-05-15 23:38:17 +0200

    Day 2

---
+++ README.md
@@ -0,0 +1,28 @@
+# `react-performance-workshop` <img src="https://user-images.githubusercontent.com/2953267/212215914-68e776eb-ed5f-439a-85dd-d31759a03196.svg" width="6">
+
+Welcome! This is the material for Day 2: Expensive Renders & Effects.
+
+Fetch the latest changes and switch to the `day2` branch:
+
+```sh
+git pull
+git switch day2
+```
+
+Then, start the app:
+
+```sh
+cd notes
+yarn
+yarn start
+```
+
+```sh
+cd widgets-editor
+yarn
+yarn start
+```
+
+⌘⌘⌘
+
+Want your own workshop? → [PerfPerfPerf](https://3perf.com)
---
+++ notes/src/utils/storage.js
@@ -0,0 +1,64 @@
+import { formatISO, parseISO } from "date-fns";
+import { parse } from "marked";
+
+export const loadNotesFromLocalStorage = () => {
+  const parsedNotes = JSON.parse(localStorage.reactWorkshopAppNotes || "{}");
+
+  const transformedNotes = {};
+  for (const [id, note] of Object.entries(parsedNotes)) {
+    const transformedNote = { ...note, date: parseISO(note.date) };
+    transformedNotes[id] = transformedNote;
+  }
+
+  return transformedNotes;
+};
+
+export const saveNotesToLocalStorage = (notes) => {
+  const transformedNotes = {};
+  for (const [id, note] of Object.entries(notes)) {
+    const transformedNote = {
+      ...note,
+      html: parse(note.text),
+      date: formatISO(note.date),
+    };
+    transformedNotes[id] = transformedNote;
+  }
+
+  const stringifiedNotes = JSON.stringify(transformedNotes);
+
+  localStorage.reactWorkshopAppNotes = stringifiedNotes;
+};
+
+let notes = loadNotesFromLocalStorage();
+
+export const getNotes = () => {
+  return notes;
+};
+
+export const putNote = (noteId, { text, date }) => {
+  if (notes[noteId]) {
+    // The note already exists; just update it
+    notes = {
+      ...notes,
+      [noteId]: {
+        ...notes[noteId],
+        text: text || notes[noteId].text,
+        date: date || notes[noteId].date,
+      },
+    };
+  } else {
+    // The note doesn’t exist; create it, filling the creation date
+    notes = {
+      ...notes,
+      [noteId]: {
+        id: noteId,
+        text: text,
+        date: date || new Date(),
+      },
+    };
+  }
+};
+
+export const deleteNotes = () => {
+  notes = {};
+};
