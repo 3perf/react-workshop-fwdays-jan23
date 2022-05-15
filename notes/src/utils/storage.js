009e425f3707a3a76e555b63c2e46d832e3fe893
Ivan Akulov <mail@iamakulov.com>
2022-05-15 23:38:17 +0200

    Day 1

---
+++ notes/src/utils/storage.js
@@ -0,0 +1,67 @@
+import { formatISO, parseISO } from "date-fns";
+
+let notes;
+
+const loadNotesFromLocalStorage = () => {
+  const parsedNotes = JSON.parse(localStorage.reactWorkshopAppNotes || "{}");
+
+  const transformedNotes = {};
+  for (const [id, note] of Object.entries(parsedNotes)) {
+    const transformedNote = { ...note, date: parseISO(note.date) };
+    transformedNotes[id] = transformedNote;
+  }
+
+  return transformedNotes;
+};
+
+const saveNotesToLocalStorage = (notes) => {
+  const transformedNotes = {};
+  for (const [id, note] of Object.entries(notes)) {
+    const transformedNote = { ...note, date: formatISO(note.date) };
+    transformedNotes[id] = transformedNote;
+  }
+
+  const stringifiedNotes = JSON.stringify(transformedNotes);
+
+  localStorage.reactWorkshopAppNotes = stringifiedNotes;
+};
+
+export const getNotes = () => {
+  if (!notes) {
+    notes = loadNotesFromLocalStorage();
+  }
+
+  return notes;
+};
+
+export const putNote = (noteId, { text, date }) => {
+  if (notes[noteId]) {
+    // The note already exists; just update it
+    notes = {
+      ...notes,
+      [noteId]: {
+        ...notes[noteId],
+        text: text || notes[noteId].text,
+        date: date || notes[noteId].date,
+      },
+    };
+  } else {
+    // The note doesn’t exist; create it, filling the creation date
+    notes = {
+      ...notes,
+      [noteId]: {
+        id: noteId,
+        text: text,
+        date: date || new Date(),
+      },
+    };
+  }
+
+  saveNotesToLocalStorage(notes);
+};
+
+export const deleteNotes = () => {
+  notes = {};
+
+  saveNotesToLocalStorage(notes);
+};
